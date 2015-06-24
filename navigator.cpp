993cf075075b13ccbace0e19f3ad2753bd4f0b22
NiklasRolleberg <niklasr93@gmail.com>
2015-06-24 14:50:58 +0200

    saker funkar lite

--- navigator.cpp
+++ navigator.cpp
@@ -1,11 +1,12 @@
 //source file
-#include "navigator.hpp" 
+#include "navigator.hpp"
 #include <iostream>
 
 Navigator::Navigator(int arg1, int arg2, int arg3)
 {
   std::cout << "constructor" << std::endl;
 }
+
 
 void Navigator::test(int arg)
 {
--- navigator.hpp
+++ navigator.hpp
@@ -1,9 +1,11 @@
 //Header
+#ifndef NAVIGATOR_H
+#define NAVIGATOR_H
 
 class Navigator
 {
 public:
-Navigator(int arg1, int arg2, int arg3);
-void test(int arg);
-  
+    Navigator(int arg1, int arg2, int arg3);
+    void test(int arg);
 };
+#endif
